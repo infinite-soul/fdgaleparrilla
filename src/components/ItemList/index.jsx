6889d90aec82492ca07cb1fcd5cbffc484589305
Luis Alejandro Gallego Ching <lordchingzo@gmail.com>
2023-06-04 18:40:36 -0500

    Daño intencional absoluto al quitar la carpeta components por completo, para resubirla pues git no es case sensitive y no actualiza

--- src/components/ItemList/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Item from "../Item";
-
-const ItemList = ({ data = [] }) => {
-	return data.map((plato) => <Item key={plato.id} info={plato} />);
-};
-
-export default ItemList;
